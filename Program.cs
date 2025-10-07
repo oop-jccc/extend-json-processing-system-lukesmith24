7e1497f1f765b86625709d4fe67229716818f2f9
lukesmith24 <wellsvilleluke@gmail.com>
2025-10-07 01:03:21 +0000

    Created an ItemProcessor and an item. Then uncommented line in program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ public static class Program
 
         builder.Services.AddSingleton<IJsonProcessor, UserJsonProcessor>();
         builder.Services.AddSingleton<IJsonProcessor, ProductJsonProcessor>();
-       // TODO: builder.Services.AddSingleton<IJsonProcessor, TodoItemProcessor>();
+        builder.Services.AddSingleton<IJsonProcessor, TodoItemProcessor>();
 
         var app = builder.Build();
 
